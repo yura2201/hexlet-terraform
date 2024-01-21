45283eeaae52be1535cc5b8f35019d5f0595c8e6
yurait6 <yurait6@gmail.com>
2024-01-21 22:44:51 +0300

    lesson 7: add load balancer
    * переделал с использованием yandex_compute_instance_group

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.47.0"
     }
     datadog = {
       source = "DataDog/datadog"
@@ -24,7 +25,7 @@ variable "folder_id" {
   sensitive = true
 }
 
-variable "yc_zone" {}
+variable "yc_main_zone" {}
 variable "os_image" {}
 
 provider "yandex" {
@@ -32,28 +33,80 @@ provider "yandex" {
   #  service_account_key_file = "path_to_service_account_key_file"
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
-  zone      = var.yc_zone
+  zone      = var.yc_main_zone
 }
 
-# https://cloud.yandex.com/ru/docs/compute/concepts/disk
-resource "yandex_compute_disk" "boot-disk-1" {
-  name = "disk-1"
-  type = "network-hdd"
-  zone = var.yc_zone
-  # GiB
-  size = "8"
-  # yc compute image list --folder-id standard-images
-  image_id = var.os_image
+variable "yc_account_id" {
+  type      = string
+  sensitive = true
 }
 
-resource "yandex_compute_disk" "boot-disk-2" {
-  name = "disk-2"
-  type = "network-hdd"
-  zone = var.yc_zone
-  # GiB
-  size = "8"
-  # yc compute image list --folder-id standard-images
-  image_id = var.os_image
+variable "yc_zones" {
+  type = list(string)
+}
+
+variable "yc_user" {
+  type      = string
+  sensitive = true
+}
+
+resource "yandex_compute_instance_group" "yandex-student-instance-group" {
+  name               = "nlb-vm-group"
+  folder_id          = var.folder_id
+  service_account_id = var.yc_account_id
+
+  instance_template {
+    platform_id = "standard-v1"
+
+    boot_disk {
+      mode = "READ_WRITE"
+      initialize_params {
+        image_id = var.os_image
+        type     = "network-hdd"
+        size     = 8
+      }
+    }
+
+    network_interface {
+      network_id = yandex_vpc_network.network-1.id
+      subnet_ids = [yandex_vpc_subnet.subnet-1.id, yandex_vpc_subnet.subnet-2.id]
+      nat        = true
+    }
+
+    resources {
+      core_fraction = 5
+      cores         = 2
+      memory        = 2
+    }
+
+    # прерываемая
+    scheduling_policy {
+      preemptible = true
+    }
+
+    metadata = {
+      user-data = "#cloud-config\nusers:\n  - name: ${var.yc_user}\n    groups: sudo\n    shell: /bin/bash\n    sudo: 'ALL=(ALL) NOPASSWD:ALL'\n    ssh-authorized-keys:\n      - ${file("~/.ssh/id/yandex/cloud/id_student_ed25519.pub")}"
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+
+  allocation_policy {
+    zones = var.yc_zones
+  }
+
+  deploy_policy {
+    max_unavailable = 1
+    max_expansion   = 0
+  }
+
+  load_balancer {
+    target_group_name = "yandex-student-target-group"
+  }
 }
 
 resource "yandex_vpc_network" "network-1" {
@@ -62,91 +115,36 @@ resource "yandex_vpc_network" "network-1" {
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "yandex-student-subnet-1"
-  zone           = var.yc_zone
+  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
+  v4_cidr_blocks = ["192.168.1.0/24"]
 }
 
-resource "yandex_compute_instance" "web-1" {
-  name = "yandex-student"
-  # https://cloud.yandex.ru/ru/docs/compute/concepts/vm-platforms
-  platform_id = "standard-v1"
-  zone        = var.yc_zone
-
-  resources {
-    # Гарантированная доля vCPU, %
-    core_fraction = 5
-    cores         = 2
-    # GiB
-    memory = 2
-  }
-
-  boot_disk {
-    disk_id = yandex_compute_disk.boot-disk-1.id
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  # прерываемая
-  scheduling_policy {
-    preemptible = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id/yandex/cloud/id_student_ed25519.pub")}"
-  }
+resource "yandex_vpc_subnet" "subnet-2" {
+  name           = "yandex-student-subnet-2"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.2.0/24"]
 }
 
-resource "yandex_compute_instance" "web-2" {
-  name = "yandex-student-2"
-  # https://cloud.yandex.ru/ru/docs/compute/concepts/vm-platforms
-  platform_id = "standard-v1"
-  zone        = var.yc_zone
-
-  resources {
-    # Гарантированная доля vCPU, %
-    core_fraction = 5
-    cores         = 2
-    # GiB
-    memory = 2
+resource "yandex_lb_network_load_balancer" "balancer" {
+  name = "yandex-student-load-balancer"
+  listener {
+    name        = "yandex-student-listener"
+    port        = 80
+    target_port = 80
   }
-
-  boot_disk {
-    disk_id = yandex_compute_disk.boot-disk-2.id
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.yandex-student-instance-group.load_balancer.0.target_group_id
+    healthcheck {
+      name                = "health-check-1"
+      unhealthy_threshold = 5
+      healthy_threshold   = 5
+      http_options {
+        port = 80
+      }
+    }
   }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  # прерываемая
-  scheduling_policy {
-    preemptible = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id/yandex/cloud/id_student_2_ed25519.pub")}"
-  }
-}
-
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.web-1.network_interface.0.ip_address
-}
-
-output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.web-2.network_interface.0.ip_address
-}
-
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.web-1.network_interface.0.nat_ip_address
-}
-
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.web-2.network_interface.0.nat_ip_address
 }
 
 ###################
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
-yc_zone      = "ru-central1-a"
+yc_zones     = ["ru-central1-a", "ru-central1-b"]
+yc_main_zone = "ru-central1-a"
 os_image     = "fd8bkgba66kkf9eenpkb"
