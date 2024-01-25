3faa7bdba15727f31336ffa650bc19dfdac4ad5d
yurait6 <yurait6@gmail.com>
2024-01-25 22:22:36 +0300

    lesson 7: add db cluster
    * cluster, db-user, host connections

--- main.tf
+++ main.tf
@@ -27,6 +27,26 @@ variable "folder_id" {
 variable "yc_zone" {}
 variable "os_image" {}
 
+variable "yc_user" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_name" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_user" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
 provider "yandex" {
   token = var.token
   #  service_account_key_file = "path_to_service_account_key_file"
@@ -96,7 +116,14 @@ resource "yandex_compute_instance" "web-1" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id/yandex/cloud/id_student_ed25519.pub")}"
+    ssh-keys = "${var.yc_user}:${file("~/.ssh/id/yandex/cloud/id_student_ed25519.pub")}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = var.yc_user
+    private_key = file("~/.ssh/id/yandex/cloud/id_student_ed25519.pub")
+    host        = self.network_interface[0].nat_ip_address
   }
 }
 
@@ -129,7 +156,14 @@ resource "yandex_compute_instance" "web-2" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id/yandex/cloud/id_student_2_ed25519.pub")}"
+    ssh-keys = "${var.yc_user}:${file("~/.ssh/id/yandex/cloud/id_student_2_ed25519.pub")}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = var.yc_user
+    private_key = file("~/.ssh/id/yandex/cloud/id_student_2_ed25519.pub")
+    host        = self.network_interface[0].nat_ip_address
   }
 }
 
@@ -137,13 +171,13 @@ resource "yandex_lb_target_group" "target-group" {
   name = "yandex-student-target-group"
 
   target {
-    subnet_id  = yandex_vpc_subnet.subnet-1.id
-    address = yandex_compute_instance.web-1.network_interface.0.ip_address
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    address   = yandex_compute_instance.web-1.network_interface.0.ip_address
   }
 
   target {
-    subnet_id  = yandex_vpc_subnet.subnet-1.id
-    address = yandex_compute_instance.web-2.network_interface.0.ip_address
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    address   = yandex_compute_instance.web-2.network_interface.0.ip_address
   }
 }
 
@@ -165,6 +199,55 @@ resource "yandex_lb_network_load_balancer" "balancer" {
       }
     }
   }
+}
+
+variable "yc_postgresql_version" {
+  type = number
+}
+
+resource "yandex_mdb_postgresql_cluster" "pg-cluster" {
+  name        = "student-pg-cluster"
+  environment = "PRESTABLE"
+  network_id  = yandex_vpc_network.network-1.id
+
+  config {
+    version = var.yc_postgresql_version
+    resources {
+      resource_preset_id = "s2.micro"
+      disk_type_id       = "network-ssd"
+      disk_size          = 15
+    }
+    postgresql_config = {
+      max_connections = 100
+    }
+  }
+
+  maintenance_window {
+    type = "WEEKLY"
+    day  = "SAT"
+    hour = 12
+  }
+
+  host {
+    zone      = var.yc_zone
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+  }
+}
+
+resource "yandex_mdb_postgresql_user" "db-user" {
+  cluster_id = yandex_mdb_postgresql_cluster.pg-cluster.id
+  name       = var.db_user
+  password   = var.db_password
+  depends_on = [yandex_mdb_postgresql_cluster.pg-cluster]
+}
+
+resource "yandex_mdb_postgresql_database" "db" {
+  cluster_id = yandex_mdb_postgresql_cluster.pg-cluster.id
+  name       = var.db_name
+  owner      = yandex_mdb_postgresql_user.db-user.name
+  lc_collate = "en_US.UTF-8"
+  lc_type    = "en_US.UTF-8"
+  depends_on = [yandex_mdb_postgresql_cluster.pg-cluster]
 }
 
 output "internal_ip_address_vm_1" {
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
-yc_zone  = "ru-central1-a"
-os_image = "fd8bkgba66kkf9eenpkb"
+yc_zone               = "ru-central1-a"
+os_image              = "fd8bkgba66kkf9eenpkb"
+yc_postgresql_version = 15
