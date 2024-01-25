57c4bf517b17c5025dbf771271770cd258e4838a
yurait6 <yurait6@gmail.com>
2024-01-25 21:34:29 +0300

    lesson 7: add load balancer
    * ВАЖНО: имя ресурса целевой группы для Network Load Balancer: yandex_LB_target_group, т.к. _ALB_ - это ресурсы для Application Load Balancer!!!

--- main.tf
+++ main.tf
@@ -133,17 +133,37 @@ resource "yandex_compute_instance" "web-2" {
   }
 }
 
-resource "yandex_alb_target_group" "target-group" {
+resource "yandex_lb_target_group" "target-group" {
   name = "yandex-student-target-group"
 
   target {
     subnet_id  = yandex_vpc_subnet.subnet-1.id
-    ip_address = yandex_compute_instance.web-1.network_interface.0.ip_address
+    address = yandex_compute_instance.web-1.network_interface.0.ip_address
   }
 
   target {
     subnet_id  = yandex_vpc_subnet.subnet-1.id
-    ip_address = yandex_compute_instance.web-2.network_interface.0.ip_address
+    address = yandex_compute_instance.web-2.network_interface.0.ip_address
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "balancer" {
+  name = "yandex-student-load-balancer"
+  listener {
+    name        = "yandex-student-listener"
+    port        = 80
+    target_port = 80
+  }
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.target-group.id
+    healthcheck {
+      name                = "health-check-1"
+      unhealthy_threshold = 5
+      healthy_threshold   = 5
+      http_options {
+        port = 80
+      }
+    }
   }
 }
 
